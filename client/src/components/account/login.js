ae816839f1ac1d48efbfdbc796a92a2463f9cf66
sakshi-bhatt-30 <129189519+sakshi-bhatt-30@users.noreply.github.com>
2024-06-06 00:34:16 +0530

    Login Page

---
+++ client/src/components/account/login.js
@@ -0,0 +1,109 @@
+import {Box, Button, TextField, styled,Typography} from '@mui/material';
+import React, { useState } from 'react';
+
+const Component = styled(Box)`
+    width: 400px;
+    margin: auto;
+   
+`;
+
+const Image = styled('img')({
+    width: 300,
+    display: 'flex',
+    margin: 'auto',
+    
+});
+const Wrapper = styled(Box)`
+    padding: 25px 35px;
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    flex-direction: column;
+    & > div, & > button, & > p {
+        margin-bottom: 30px;
+    }
+`;
+const LoginButton = styled(Button)`
+    
+    background: #8FBC8B;
+    color: #1B4D3E;
+    height: 48px;
+    border-radius: 2px;
+    
+`;
+
+const SignupButton = styled(Button)`
+    
+    background: #fff;
+    color: #2874f0;
+    height: 48px;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+`;
+
+const Text = styled(Typography)`
+    color: #878787;
+    font-size: 12px;
+`;
+
+// const Error = styled(Typography)`
+//     font-size: 10px;
+//     color: #ff6161;
+//     line-height: 0;
+//     margin-top: 10px;
+//     font-weight: 600;
+// `;
+
+const signupInitial ={
+    name:'',
+    username:'',
+    password:''
+}
+
+
+
+const Login = () => {
+    
+    const imageURL = 'https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png';
+
+    const [account,toggleAccount]= useState('login');
+    const[signup,setSignup]=useState(signupInitial)
+
+    const toggleSignup = () => {
+        account === 'signup' ? toggleAccount('login') : toggleAccount('signup');
+    }
+    const onInputChange =(e) =>{
+        setSignup({...signup, [e.target.name]: e.target.value});
+    }
+
+    return ( 
+        <Component>
+            <Box>
+                <Image src={imageURL} alt="Login"/>
+                {
+                account === 'login' ?
+                <Wrapper>
+                    <TextField variant="standard" label='Enter Username'/>
+                    <TextField variant="standard" label="Enter Password"/>
+                    <LoginButton variant="contained" >Login</LoginButton>
+                    <Text style={{ textAlign: 'center' }}>OR</Text>
+                    <SignupButton onClick={() => toggleSignup()}>Sign Up</SignupButton>
+                </Wrapper> 
+                :
+                <Wrapper>
+                    <TextField variant="standard" onChange={(e)=>onInputChange(e)} name="name" label='Enter Name' />
+                    <TextField variant="standard" onChange={(e)=>onInputChange(e)} name="username" label='Enter Username' />
+                    <TextField variant="standard" onChange={(e)=>onInputChange(e)} name="password" label='Enter Password' />
+
+                    <SignupButton >Signup</SignupButton>
+                    <Text style={{ textAlign: 'center' }}>OR</Text>
+                    <LoginButton variant="contained" onClick={() => toggleSignup()}>Already have an account</LoginButton>
+                </Wrapper>
+                }
+                
+            </Box>
+        </Component>
+     );
+}
+ 
+export default Login;
