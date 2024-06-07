7c26714fa99750b0a271f2207e9fcae9dc9fe90c
sakshi-bhatt-30 <129189519+sakshi-bhatt-30@users.noreply.github.com>
2024-06-07 22:37:24 +0530

    constant messages

---
+++ client/src/constants/config.js
@@ -0,0 +1,23 @@
+export const API_NOTIFICATION_MESSAGE={
+    loading:{
+        title: 'loading...',
+        message:'Data id being loaded. Wait for a while'
+    },
+    success:{
+        title:'Success',
+        message:'Data loaded'
+    },
+    responseFailure:{
+        title:'Error',
+        message:'An error occurred while fetching the response from the server'
+    },
+    requestFail:{
+        title:'Error',
+        message:'An error occurred while parsing the request data'     
+    },
+    networkError:{
+        title:'Error',
+        message:'Unable to connect with the server. Please check your internet connection'
+    }
+
+}
