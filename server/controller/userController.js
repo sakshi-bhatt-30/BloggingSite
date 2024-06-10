1c1e10c24949084246135497351f8213ea2cb66a
sakshi-bhatt-30 <129189519+sakshi-bhatt-30@users.noreply.github.com>
2024-06-10 15:55:53 +0530

    backend updated

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,10 +1,13 @@
 import User from '../model/user.js'
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import Token from '../model/token.js';
 
 
 
-
-const signupUser = async (request,response) => {
+dotenv.config();
+export const signupUser = async (request,response) => {
     try{
         const hashedPassword = await bcrypt.hash(request.body.password, 10);
 
@@ -21,4 +24,30 @@ const signupUser = async (request,response) => {
     }
 }
  
-export default signupUser;
+// export default signupUser;
+
+export const loginUser = async (request, response) => {
+    let user = await User.findOne({ username: request.body.username });
+    if (!user) {
+        return response.status(400).json({ msg: 'Username does not match' });
+    }
+
+    try {
+        let match = await bcrypt.compare(request.body.password, user.password);
+        if (match) {
+            const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, { expiresIn: '15m'});
+            const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_SECRET_KEY);
+            
+            const newToken = new Token({ token: refreshToken });
+            await newToken.save();
+        
+            response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken,name: user.name, username: user.username });
+        
+        } else {
+            response.status(400).json({ msg: 'Password does not match' })
+        }
+    } catch (error) {
+        response.status(500).json({ msg: 'error while login the user' })
+    }
+
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import signupUser from '../controller/userController.js'
+import {signupUser, loginUser} from '../controller/userController.js'
 
 const router = express.Router();
 
 router.post('/signup', signupUser);
+router.post('/login', loginUser);
 
 
 export default router;
