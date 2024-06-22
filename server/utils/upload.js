b047086222541c1ee816d4fd99d1fbcc8ecf098b
sakshi-bhatt-30 <129189519+sakshi-bhatt-30@users.noreply.github.com>
2024-06-22 15:29:15 +0530

    updated

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,10 +1,15 @@
 import multer from 'multer';
 import pkg from 'multer-gridfs-storage';
 const { GridFsStorage } = pkg;
+import dotenv from "dotenv";
+dotenv.config();
+
+const username= process.env.DB_USERNAME;
+const password=process.env.DB_PASSWORD;
 
 // Create storage engine
 const storage = new GridFsStorage({
-    url: "mongodb://sakshi:blog@ac-87pheul-shard-00-00.jt7nge4.mongodb.net:27017,ac-87pheul-shard-00-01.jt7nge4.mongodb.net:27017,ac-87pheul-shard-00-02.jt7nge4.mongodb.net:27017/?ssl=true&replicaSet=atlas-r47hfv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster",
+    url: `mongodb://${username}:${password}@ac-87pheul-shard-00-00.jt7nge4.mongodb.net:27017,ac-87pheul-shard-00-01.jt7nge4.mongodb.net:27017,ac-87pheul-shard-00-02.jt7nge4.mongodb.net:27017/?ssl=true&replicaSet=atlas-r47hfv-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster`,
     file: (request, file) => {
         const match = ["image/png", "image/jpg", "image/jpeg"];
 
